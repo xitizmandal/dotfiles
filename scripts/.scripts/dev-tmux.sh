8a1f4e46f5b486f49a629b17c0756ab47b50afd5
Kshitiz Mandal <xitizmandal@gmail.com>
2020-08-30 16:19:52 +0545

    [script] change workspace layout for envs

--- scripts/.scripts/dev-tmux.sh
+++ scripts/.scripts/dev-tmux.sh
@@ -4,10 +4,18 @@
 # tmux split-window -h 'ipython'
 # tmux attach-session -d
 
+conda_activate="conda activate"
+activate_cmd="workon -n"
+
+if [ "$2" == "conda" ]; then
+    activate_cmd=$conda_activate
+fi
+
 tmux new-window \; \
+  send-keys "$activate_cmd $1" C-m \; \
   send-keys 'vim' C-m \; \
   split-window -v -p 30 \; \
-  send-keys 'conda activate ' $1 C-m \; \
+  send-keys "$activate_cmd $1" C-m \; \
   split-window -h \; \
-  send-keys 'conda activate ' $1 C-m \; \
+  send-keys "$activate_cmd $1" C-m \; \
   send-keys 'python3' C-m \; 
