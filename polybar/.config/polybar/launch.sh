4ef3599058994f19cba97e67621eec470f609285
Kshitiz Mandal <xitizmandal@gmail.com>
2021-01-03 20:17:33 +0545

    [polybar] reload flag to the launch script

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -15,7 +15,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d " " -f1);do
-        MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+        MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload main &
     done
 else
     polybar -c ~/.config/polybar/config.ini main &
