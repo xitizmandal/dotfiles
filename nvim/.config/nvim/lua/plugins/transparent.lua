6acbe9709a61687e1d61a846770ece319a426e12
Kshitiz Mandal <xitizmandal@gmail.com>
2022-05-25 22:20:22 +0545

    [nvim] add keymaps for gitsigns

---
+++ nvim/.config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,7 @@
+require("transparent").setup({
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
