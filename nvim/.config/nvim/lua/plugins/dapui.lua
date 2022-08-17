216dab6d3b20405524d752c2a05addc59be43374
Kshitiz Mandal <xitizmandal@gmail.com>
2022-08-17 22:24:43 +0545

    [nvim] config for dapui

---
+++ nvim/.config/nvim/lua/plugins/dapui.lua
@@ -0,0 +1,47 @@
+local dapui = require('dapui')
+local dap = require('dap')
+
+dapui.setup({
+    layouts = {
+        {
+            elements = {
+                -- Elements can be strings or table with id and size keys.
+                { id = "breakpoints", size = 0.25 },
+                { id = "stacks", size = 0.25 },
+                { id = "watches", size = 0.25 },
+                { id = "scopes", size = 0.25 },
+            },
+            size = 0.3, -- 40 columns
+            position = "left",
+        },
+        {
+            elements = {
+                "repl",
+                "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+        },
+    },
+    floating = {
+        max_height = nil, -- These can be integers or a float between 0 and 1. max_width = nil, -- Floats will be treated as percentage of your screen.
+        border = "single", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+            close = { "q", "<Esc>" },
+        },
+    },
+    windows = { indent = 1 },
+    render = {
+        max_type_length = nil, -- Can be integer or nil.
+    }
+})
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
