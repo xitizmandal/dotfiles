d460d78271642a433be94c42f1093e0f7ea2c032
Kshitiz Mandal <xitizmandal@gmail.com>
2023-01-25 14:43:43 +0545

    refactor telescome

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,62 +1,60 @@
 local actions = require("telescope.actions")
 require("telescope").setup({
-    defaults = require('telescope.themes').get_ivy({
+    -- defaults = require('telescope.themes').get_ivy({
+    defaults = {
         mappings = {
             n = {
                 ["jk"] = actions.close
             },
         },
-        -- layout_strategy = "horizontal",
-        -- layout_config = {
-        --     width = 0.75,
-        --     preview_cutoff = 120,
-        --     prompt_position = "bottom",
-        -- },
-
+        layout_strategy = "horizontal",
+        layout_config = {
+            width = 0.75,
+            preview_cutoff = 120,
+            prompt_position = "top",
+            preview_width = 0.5,
+        },
+        -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
         color_devicons = true,
 
         winblend = 0,
-        -- prompt_prefix = function(opts)
-        --     return string.format("AD")
-        -- end,
-        layout_strategy = "horizontal",
-        layout_config = {
-            anchor = "SW",
-            --     -- height = vim.o.lines, -- maximally available lines
-            height = 0.5, -- maximally available lines
-            width = vim.o.columns, -- maximally available columns
-            --     -- preview_cutoff = 5,
-            prompt_position = "top",
-            --     -- preview_height = 0.3, -- 60% of available lines
-        },
+        sorting_strategy = "ascending",
         -- preview = false,
-        extensions = {
-            fzf = {
-                fuzzy = true, -- false will only do exact matching
-                override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                -- the default case_mode is "smart_case"
-            },
-            file_browser = {
-                theme = "ivy",
-                -- disables netrw and use telescope-file-browser in its place
-                hijack_netrw = true,
-                -- mappings = {
-                --     ["i"] = {
-                --         -- your custom insert mode mappings
-                --     },
-                --     ["n"] = {
-                --         -- your custom normal mode mappings
-                --     },
-                -- },
-            },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
         },
-    }
-    ),
+        file_browser = {
+            -- theme = "ivy",
+            layout_strategy = "vertical",
+            -- layout_config = {
+            --     anchor = "SW",
+            --     height = 0.3,
+            --     width = vim.o.columns,
+            --     preview_height = 0,
+            -- },
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            preview = false,
+            -- mappings = {
+            --     ["i"] = {
+            --         -- your custom insert mode mappings
+            --     },
+            --     ["n"] = {
+            --         -- your custom normal mode mappings
+            --     },
+            -- },
+        },
+    },
 })
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
+require('telescope').load_extension('dap')
 
 local builtin = require('telescope.builtin')
 
@@ -65,7 +63,7 @@ vim.keymap.set('n', '<leader>fc', builtin.git_files, { desc = '[F]uzzy [C]' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]uzzy [B]uffers' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]uzzy [G]rep' })
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = '[F]uzzy [S]tring' })
-vim.keymap.set('n', '<leader>ft', require("telescope").extensions.file_browser.file_browser, { desc='[F]uzzy [T]ree'})
+vim.keymap.set('n', '<leader>ft', require("telescope").extensions.file_browser.file_browser, { desc = '[F]uzzy [T]ree' })
 
 vim.keymap.set('n', '<leader>/', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,16 +1,20 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "help", "python", "javascript", "typescript", "lua", "rust" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = { "c", "help", "python", "javascript", "typescript", "lua", "rust" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = false,
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+        disable = {"python"}
+    },
 }
