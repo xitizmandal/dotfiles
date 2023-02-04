f6e5602d3186d066984dab57e9e91a6eeaee4312
Kshitiz Mandal <xitizmandal@gmail.com>
2023-02-04 20:27:09 +0545

    Add nvim custom keymaps

---
+++ nvim/.config/nvim/after/plugin/diffview.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>do', ":DiffviewOpen<CR>", { desc = '[D]iffview [O]pen' })
+vim.keymap.set('n', '<leader>dc', ":DiffviewClose<CR>", { desc = '[D]iffview [C]lose' })
+vim.keymap.set('n', '<leader>dh', ":DiffviewFileHistory %<CR>", { desc = '[D]iffview File [H]istory' })
