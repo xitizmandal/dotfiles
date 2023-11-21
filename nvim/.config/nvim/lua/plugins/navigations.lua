05bf2300109a72e5baba27c8c4abfda513ca4570
Kshitiz Mandal <xitizmandal@gmail.com>
2023-11-21 17:53:44 +0545

    [nvim] add oil plugin

--- nvim/.config/nvim/lua/plugins/navigations.lua
+++ nvim/.config/nvim/lua/plugins/navigations.lua
@@ -104,6 +104,15 @@ return {
 
     },
     {
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require('oil').setup()
+        end
+    },
+    {
         'nvim-telescope/telescope.nvim',
         version = false,
         dependencies = {
