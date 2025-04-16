591454c2edb3d0cec1c75341d65e0eb95e682222
Kshitiz Mandal <xitiz.dev@gmail.com>
2025-04-16 09:39:56 -0500

    change layout of dapui

--- nvim/.config/nvim/lua/plugins/dapui.lua
+++ nvim/.config/nvim/lua/plugins/dapui.lua
@@ -8,6 +8,33 @@ return {
         local dapui = require('dapui')
 
         dapui.setup({
+            layouts = { {
+                elements = { {
+                    id = "scopes",
+                    size = 0.25
+                }, {
+                    id = "breakpoints",
+                    size = 0.25
+                }, {
+                    id = "stacks",
+                    size = 0.25
+                }, {
+                    id = "watches",
+                    size = 0.25
+                } },
+                position = "left",
+                size = 0.4
+            }, {
+                elements = { {
+                    id = "repl",
+                    size = 0.5
+                }, {
+                    id = "console",
+                    size = 0.5
+                } },
+                position = "bottom",
+                size = 10
+            } },
         })
 
         dap.listeners.after.event_initialized["dapui_config"] = function()
