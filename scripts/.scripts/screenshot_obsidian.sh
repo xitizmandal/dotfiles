e02c5634c2a185484e9a4c6091d734989e509ea5
Kshitiz Mandal <xitiz.dev@gmail.com>
2025-04-16 09:37:42 -0500

    remove linux packages

--- scripts/.scripts/screenshot_obsidian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NAME="$(rofi -dmenu -i -p 'Picture Name :')"
-PATH_TO_OBSIDIAN=$HOME/Documents/knowledge-base/attachments/
-
-complete_path="${PATH_TO_OBSIDIAN}${NAME}.png"
-
-scrot -s $complete_path
-notify-send -a "Obsidian attachment" "${NAME}.png"
--- scripts/.scripts/volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-get_volume() {
-    pactl get-sink-volume @DEFAULT_SINK@ | grep % | head -n 1 | cut -d '/' -f 2 | cut -d '%' -f 1
-}
-
-CURRENT_VOLUME="$(get_volume)"
-# echo $CURRENT_VOLUME
-dunstify -r 1100 -i audio-speaker --hints=int:value:"$CURRENT_VOLUME" -a "Volume: $CURRENT_VOLUME" ""
